e8954c8c4b0487398046fd6588df14bff2162fb0
Alan Campbell <ahcampbellster@gmail.com>
2017-12-21 15:54:04 -0700

    Modified approach to wc, which might solve the n > 1 space counting issue.

---
+++ rust/posix-utilities/src/bin/wc.rs
@@ -0,0 +1,38 @@
+use std::env;
+use std::io;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+
+fn wc(file_path: String) -> u32 {
+    let fh = match File::open(&file_path) {
+        Err(_) => panic!("File name or path is incorrect."),
+        Ok(t) => t,
+    };
+    let mut text = BufReader::new(fh);
+    let mut string = String::new();
+    let buffer = text.read_to_string(&mut string);
+    let mut temp_str = String::new();
+    let mut count = 0;
+    for i in string.chars() {
+        if i == ' ' {
+            if temp_str.len() > 0 {
+                temp_str.clear();
+                count += 1;
+            } else {
+                continue
+            }
+        } else{
+            temp_str.push(i)
+        }
+    }
+    return count;
+}
+
+
+
+
+fn main() {
+    let file_path = env::args().nth(1).unwrap();
+    println!("Word Count: {}", wc(file_path));
+}
